fd402f507211e284173d5b763ad19ef55e3826fb
Antwan <antwan@antwan.eu>
2013-01-21 11:20:42 +0100

    Using a print_r (dump) function to show return data within a <pre> tag. Nice way of presenting data.

--- api-center.php
+++ api-center.php
@@ -18,18 +18,21 @@ session_start();
             <label for="tags"></label>
             <input id="tags" placeholder="Enter tagname" name="replace" type="text"/>
             <a href="#" rel="apiTags">apiTags</a>
+			<div class="code"></div>
         </fieldset>
         <fieldset class="api-call">
             <legend>apiTagsMediaRecent</legend>
             <label for="tags-media-recent"></label>
             <input id="tags-media-recent" placeholder="Enter tagname" name="replace" type="text"/>
             <a href="#" rel="apiTagsMediaRecent">apiTagsMediaRecent</a>
+            <div class="code"></div>
         </fieldset>
         <fieldset class="api-call">
             <legend>apiTagsSearch</legend>
             <label for="tags-search"></label>
             <input id="tags-search" placeholder="Enter tagname" name="q" type="text"/>
             <a href="#" rel="apiTagsSearch">apiTagsSearch</a>
+            <div class="code"></div>
         </fieldset>
         <fieldset class="api-call">
             <legend>apiMediaSearch</legend>
@@ -38,10 +41,12 @@ session_start();
             <label for="media-search-lng"></label>
             <input id="media-search-lng" placeholder="Enter longitude" name="lng" type="text"/>
             <a href="#" rel="apiMediaSearch">apiMediaSearch</a>
+            <div class="code"></div>
         </fieldset>
         <fieldset class="api-call">
             <legend>apiMediaPopular</legend>
             <a href="#" rel="apiMediaPopular">apiMediaPopular</a>
+            <div class="code"></div>
         </fieldset>
 		<?php if (isset($_SESSION['user'])) { ?>
         <fieldset class="api-call">
@@ -49,12 +54,14 @@ session_start();
             <label for="users"></label>
             <input id="users" placeholder="Enter user ID" name="replace" value="<?php print $_SESSION['user']['id']; ?>" type="text"/>
             <a href="#" rel="apiUsers">apiUsers</a>
+            <div class="code"></div>
         </fieldset>
         <fieldset class="api-call">
             <legend>apiUsersSelfFeed</legend>
             <label for="users-self-feed"></label>
             <input id="users-self-feed" placeholder="Enter count to return" name="count" type="text"/>
             <a href="#" rel="apiUsersSelfFeed">apiUsersSelfFeed</a>
+            <div class="code"></div>
         </fieldset>
         <fieldset class="api-call">
             <legend>apiUsersMediaRecent</legend>
@@ -63,18 +70,21 @@ session_start();
             <label for="users-media-recent-count"></label>
             <input id="users-media-recent-count" placeholder="Enter count to return" name="count" type="text"/>
             <a href="#" rel="apiUsersMediaRecent">apiUsersMediaRecent</a>
+            <div class="code"></div>
         </fieldset>
         <fieldset class="api-call">
             <legend>apiUsersSelfMediaLiked</legend>
             <label for="users-self-media-liked"></label>
             <input id="users-self-media-liked" placeholder="Enter count to return" name="count" type="text"/>
             <a href="#" rel="apiUsersSelfMediaLiked">apiUsersSelfMediaLiked</a>
+            <div class="code"></div>
         </fieldset>
         <fieldset class="api-call">
             <legend>apiUsersSearch</legend>
             <label for="users-search"></label>
             <input id="users-search" placeholder="Enter a name" name="q" type="text"/>
             <a href="#" rel="apiUsersSearch">apiUsersSearch</a>
+            <div class="code"></div>
         </fieldset>
 		<?php } ?>
     </form>
--- js/api-center.js
+++ js/api-center.js
@@ -1,3 +1,5 @@
+var currentApiCallFieldset = null;
+
 $(document).ready(init);
 
 /**
@@ -15,11 +17,13 @@ function init() {
  * @return {Boolean}
  */
 function formatApiData(event) {
+    $('.api-call .code').hide();
+    currentApiCallFieldset = $(this);
     var replace = '';
     var params = {};
 
     //Loop through all the fields in this set & add data parameters
-    $(event.currentTarget).siblings('input').each(function () {
+    currentApiCallFieldset.siblings('input').each(function () {
         var name = $(this).attr('name');
         if (name == 'replace') {
             replace = $(this).val();
@@ -28,7 +32,7 @@ function formatApiData(event) {
         }
     });
 
-    var method = $(event.currentTarget).attr('rel');
+    var method = currentApiCallFieldset.attr('rel');
     var arguments = JSON.stringify({
         replace:replace,
         params:params
@@ -63,6 +67,9 @@ function apiCall(method, arguments) {
  * @param data
  */
 function apiCallSuccessHandler(data) {
+    var pre = $("<pre>");
+    pre.text(dump(data));
+    currentApiCallFieldset.siblings('.code').show().append(pre);
     console.log(data);
 }
 
@@ -78,8 +85,8 @@ function apiCallErrorHandler(error) {
 /**
  * Ask user permission if GeoLocation functionality is available
  */
-function geoLocationAskPermission(){
-    if (navigator.geolocation){
+function geoLocationAskPermission() {
+    if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(geoLocationHandler);
     }
 }
@@ -89,7 +96,44 @@ function geoLocationAskPermission(){
  *
  * @param data
  */
-function geoLocationHandler(data){
+function geoLocationHandler(data) {
     $('#media-search-lat').val(data.coords.latitude);
     $('#media-search-lng').val(data.coords.longitude);
+}
+
+/**
+ * Function : dump()
+ * Arguments: The data - array,hash(associative array),object
+ *    The level - OPTIONAL
+ * Returns  : The textual representation of the array.
+ * This function was inspired by the print_r function of PHP.
+ * This will accept some data as the argument and return a
+ * text that will be a more readable version of the
+ * array/hash/object that is given.
+ * Docs: http://www.openjs.com/scripts/others/dump_function_php_print_r.php
+ */
+function dump(arr, level) {
+    var dumped_text = "";
+    if (!level) level = 0;
+
+    //The padding given at the beginning of the line.
+    var level_padding = "";
+    for (var j = 0; j < level + 1; j++) level_padding += "    ";
+
+    if (typeof(arr) == 'object') { //Array/Hashes/Objects
+        for (var item in arr) {
+            var value = arr[item];
+
+            if (typeof(value) == 'object') { //If it is an array,
+                dumped_text += level_padding + item + ": {\n";
+                dumped_text += dump(value, level + 1);
+                dumped_text += level_padding + "}\n";
+            } else {
+                dumped_text += level_padding + item + ": \"" + value + "\"\n";
+            }
+        }
+    } else { //Stings/Chars/Numbers etc.
+        dumped_text = "===>" + arr + "<===(" + typeof(arr) + ")";
+    }
+    return dumped_text;
 }
