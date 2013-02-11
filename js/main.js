43f46c1dcd395264c73f1a8dfb45d52485c466a7
Antwan <antwan@antwan.eu>
2013-02-11 12:55:52 +0100

    Refactored some code to make the title of colorbox nicer (with username & links to profile/location on google maps)

--- js/main.js
+++ js/main.js
@@ -75,42 +75,54 @@ function fetchImages() {
 /**
  * Append data to HTML & retrieve more data if we're not done yet
  *
- * @param data
+ * @param returnData
  * @see fetchImages()
  */
-function fetchImagesSuccessHandler(data) {
-    if (data.data === undefined) {
+function fetchImagesSuccessHandler(returnData) {
+    if (returnData.data === undefined) {
         return;
     }
 
     //Loop through data & append images in wrapper div
-    var instagramData = data.data;
+    var instagramData = returnData.data;
     for (var i in instagramData) {
         var captionText = instagramData[i].caption !== null ? instagramData[i].caption.text : '';
         var images = instagramData[i].images;
-        var totalLikes = instagramData[i].likes.count;
 
+        //Create template & add to gallery with colorbox options
         var imgDiv = imageTemplate(images.thumbnail.url, images.standard_resolution.url, captionText);
-        var colorBoxImg = $(imgDiv).colorbox($.extend(colorboxOptions, {title:totalLikes + ' &hearts;'}));
+        var colorBoxImg = $(imgDiv).colorbox($.extend(colorboxOptions, {title:colorBoxTitle(instagramData[i])}));
         $('#images').append(colorBoxImg);
     }
 
     //Set the ID so it can be used within the next AJAX call
-    nextMaxTagId = data.pagination.next_max_tag_id;
+    nextMaxTagId = returnData.pagination.next_max_tag_id;
 
     togglePreloader();
 }
 
 /**
- * @param response
- * @see fetchImages()
+ * Format a string for the current instagramEntry, as a title for the colorBox popup
+ *
+ * @param instagramEntry
+ * @return {String}
  */
-function fetchImagesErrorHandler(response) {
-    togglePreloader();
-    $('#load-more').hide();
+function colorBoxTitle(instagramEntry) {
+    //Get likes & username to create colorBox title
+    var totalLikes = instagramEntry.likes.count;
+    var username = instagramEntry.user.username;
+    var fullNameTitle = 'Visit profile of ' + instagramEntry.user.full_name;
+    var colorBoxTitle = totalLikes + ' &hearts; | <a title="' + fullNameTitle + '" href="http://instagram.com/' + username + '" target="_blank">@' + username + '</a>';
 
-    var responseObject = JSON.parse(response.responseText);
-    alert(responseObject.message);
+    //Add location linkmarker to colorBox title if any lat/lng are available
+    var location = instagramEntry.location;
+    if (location !== null && location.latitude && location.longitude) {
+        var googleMapsLink = 'http://maps.google.com/maps?q=' + location.latitude + ',+' + location.longitude;
+        var locationName = $.trim('Find ' + (location.name ? location.name : '')) + ' on Google Maps';
+        colorBoxTitle += ' | <a title="' + locationName + '" class="location" href="' + googleMapsLink + '" target="_blank"></a>';
+    }
+
+    return colorBoxTitle;
 }
 
 /**
@@ -123,6 +135,18 @@ function fetchImagesErrorHandler(response) {
  */
 function imageTemplate(imageUrlThumb, imageUrlFull, caption) {
     return "<img src='" + imageUrlThumb + "' href='" + imageUrlFull + "' alt='" + caption + "' title='" + caption + "' />";
+}
+
+/**
+ * @param response
+ * @see fetchImages()
+ */
+function fetchImagesErrorHandler(response) {
+    togglePreloader();
+    $('#load-more').hide();
+
+    var responseObject = JSON.parse(response.responseText);
+    alert(responseObject.message);
 }
 
 /**
